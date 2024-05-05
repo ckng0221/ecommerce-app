6e6b6b9e59cba339b88b1b13459bb70e9499e16a
CK Ng <ngchoonkhon@gmail.com>
2024-05-05 23:58:59 +0800

    add optional pagination

--- backend/utils/gorm.go
+++ backend/utils/gorm.go
@@ -8,16 +8,25 @@ import (
 	"gorm.io/gorm"
 )
 
+// Pagination
+//
+// page: default 1, page-size: default 100.
 func Paginate(r *http.Request) (func(db *gorm.DB) *gorm.DB, error) {
 	pageString := r.URL.Query().Get("page")
 	pageSizeString := r.URL.Query().Get("page-size")
-	page, error := strconv.Atoi(pageString)
-	if error != nil {
-		return nil, errors.New("invalid page")
-	}
-	pageSize, error := strconv.Atoi(pageSizeString)
-	if error != nil {
-		return nil, errors.New("invalid page size")
+	var page int
+	var pageSize int
+	var error error
+
+	if pageString != "" && pageSizeString != "" {
+		page, error = strconv.Atoi(pageString)
+		if error != nil {
+			return nil, errors.New("invalid page")
+		}
+		pageSize, error = strconv.Atoi(pageSizeString)
+		if error != nil {
+			return nil, errors.New("invalid page size")
+		}
 	}
 
 	if page <= 0 {
